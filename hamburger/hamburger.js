00e3024f6975a82fa70d31da1089d5d11983e71e
jin <jin200126@gmail.com>
2023-08-10 08:59:35 +0900

    hamburger

---
+++ hamburger/hamburger.js
@@ -0,0 +1,10 @@
+const nav_menu = document.querySelector(".nav_menu");
+const hamburger_button = document.querySelector('.hamburger_logo');
+
+hamburger_button.addEventListener('click',function(){
+    nav_menu.classList.toggle('show');
+})
+
+
+
+
