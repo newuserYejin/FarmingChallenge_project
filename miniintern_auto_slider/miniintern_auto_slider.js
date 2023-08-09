70932968d6df45d3da3523bf189d741ae347caf8
jin <jin200126@gmail.com>
2023-08-09 17:12:42 +0900

    miniintern auto_slide

---
+++ miniintern_auto_slider/miniintern_auto_slider.js
@@ -0,0 +1,69 @@
+const slider = document.querySelector('.slider');
+
+const leftArrow = document.querySelector('.left');
+const rightArrow = document.querySelector('.right');
+
+const indicatorParents = document.querySelector('.controls ul');
+
+var sectionIndex = 0;
+
+function setIndex(){
+    document.querySelector('.controls .selected').classList.remove('selected');
+    slider.style.transform = 'translate(' + (sectionIndex) * (-25) + '%)';
+}
+
+rightArrow.addEventListener('click',function(){
+    sectionIndex = (sectionIndex < 3) ? sectionIndex + 1 :0;
+    setIndex();
+    indicatorParents.children[sectionIndex].classList.add('selected');      //화살표로 이미지 움직이면 밑에 버튼에 맞춰서 selected추가
+});
+
+
+leftArrow.addEventListener('click',function(){
+    sectionIndex = (sectionIndex > 0) ? sectionIndex - 1 :3;
+    setIndex();
+    indicatorParents.children[sectionIndex].classList.add('selected');
+});
+
+
+//밑에 버튼
+
+document.querySelectorAll('.controls li').forEach(function(indicator, index){
+    indicator.addEventListener('click',function(){
+        sectionIndex = index;
+        setIndex()
+        indicator.classList.add('selected');
+    });
+});
+
+// 화살표나 밑에 마우스가 올라가면 자동 슬라이드 멈추기
+let pointer = undefined
+
+function move(){
+    if(pointer === undefined){
+        pointer = setInterval(function() {
+            sectionIndex = (sectionIndex < 3) ? sectionIndex + 1 :0;
+            setIndex();
+            indicatorParents.children[sectionIndex].classList.add('selected');      //화살표로 이미지 움직이면 밑에 버튼에 맞춰서 selected추가
+        },3000);
+    }
+}
+
+move();
+
+function stop(){
+    clearInterval(pointer);
+    pointer = undefined;
+
+    console.log(pointer);
+}
+
+const point = document.querySelector('.controls')
+
+point.addEventListener('mouseenter', function(){
+    stop();
+})
+
+point.addEventListener('mouseleave',function(){
+    move();
+})
